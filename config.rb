46a1779a531a34744290a9eaea58d08f3b48af37
justingood <jgood.hrss@gmail.com>
2016-05-08 15:53:38 -0400

    minimizing config updates

--- config.rb
+++ config.rb
@@ -78,19 +78,19 @@ activate :directory_indexes
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  set :http_prefix, "/Content/images/"
   activate :gzip
   activate :minify_html
   activate :imageoptim
