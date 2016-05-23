06a3b781ab2f15986b72ff6ec59c0d48a7666b53
Landmark Property Services, Inc <sschaefer@landmark-property.com>
2016-05-23 18:09:03 -0400

    changed after_build to true in order to test the sync3 issue

--- config.rb
+++ config.rb
@@ -109,8 +109,8 @@ end
   activate :s3_sync do |s3_sync|
       s3_sync.bucket                     = ENV['AWS_BUCKET'] || 'landmark-property.com'
       s3_sync.region                     = 'us-east-1'
-      s3_sync.delete                     = false
-      s3_sync.after_build                = true
+      s3_sync.delete                     = true
+      s3_sync.after_build                = false
       s3_sync.prefer_gzip                = false
       s3_sync.path_style                 = true
       s3_sync.reduced_redundancy_storage = false
